3d79bdaa6a3a88c95d82acf1831255032a2af787
sennett <qaplwsokedijrfuhtg6>
2019-08-18 17:44:16 -0500

    board sets a piece at the start

--- src/game/Board.test.ts
+++ src/game/Board.test.ts
@@ -1,13 +1,22 @@
 import Board from './Board'
+import Substitute, { SubstituteOf } from '@fluffy-spoon/substitute'
+import IPiece from './IPiece'
 
 describe('Board', () => {
+  let board: Board
+  beforeEach(() => {
+    board = new Board()
+  })
   it('can be created', () => {
-    let board = new Board()
     expect(board).toBeInstanceOf(Board)
   })
 
-  it('takes the pieces', () => {
-    let board = new Board()
-    expect(board).toBeInstanceOf(Board)
+  describe('setPiece', () => {
+    it('sets the piece at the start', () => {
+      const mockPiece: SubstituteOf<IPiece> = Substitute.for<IPiece>()
+      board.setPiece(mockPiece)
+      expect(board.state.length).toBe(1)
+      expect(board.state[0].position).toBe(1)
+    })
   })
 })
--- src/game/Board.ts
+++ src/game/Board.ts
@@ -1,10 +1,31 @@
 import IBoard from './IBoard'
 import IPiece from './IPiece'
 
+const START_POSITION = 1
+const BOARD_LENGTH = 100
+
 class Board implements IBoard {
-  setPiece (piece: IPiece): void {
+  private pieces: Array<IPiecePosition> = new Array<IPiecePosition>()
+
+  movePiece (piece: IPiece, spaces: number): void {
     throw new Error('Method not implemented.')
   }
+
+  setPiece (piece: IPiece): void {
+    this.pieces.push({
+      piece,
+      position: START_POSITION
+    })
+  }
+
+  get state (): Array<IPiecePosition> {
+    return this.pieces
+  }
+}
+
+interface IPiecePosition {
+  position: number
+  piece: IPiece
 }
 
 export default Board
